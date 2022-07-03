e9506c5cf808a64eedae75454317062ca65b52f3
victorneves99 <victorneves99@gmail.com>
2022-07-02 22:29:21 -0300

    CRUD

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
 //query params = ?nome=NodeJs
 
 //Route params = /curso/2
@@ -25,6 +27,32 @@ server.get("/cursos/:index", (req, res) => {
   const { index } = req.params;
 
   return res.send(cursos[index]);
+});
+server.get("/cursos", (req, res) => {
+  return res.json(cursos);
+});
+server.post("/cursos", (req, res) => {
+  const { name } = req.body;
+
+  cursos.push(name);
+
+  return res.json(cursos);
+});
+server.put("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  const { name } = req.body;
+
+  cursos[index] = name;
+
+  return res.json(cursos);
+});
+server.delete("/cursos/:index", (req, res) => {
+  const { index } = req.params;
+
+  cursos.splice(index, 1);
+
+  return res.json({ message: "Curso deletado com sucesso" });
 });
 
 server.listen(3000);
