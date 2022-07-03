ef3f45a3bbbe4a217be191f47bb66e4dbe391f9e
victorneves99 <victorneves99@gmail.com>
2022-07-02 21:51:00 -0300

    query,route Params.

--- index.js
+++ index.js
@@ -2,9 +2,22 @@ const express = require("express");
 
 const server = express();
 
+//query params = ?nome=NodeJs
+
+//Route params = /curso/2
+
+//request Body = {nomew:"NodeJS',tipo:'Back-end'"}
+
 //localhost:3000/curso
-server.get("/curso", () => {
-  console.log("Acessou rota");
+server.get("/curso/:id", (req, res) => {
+  const id = req.params.id;
+
+  return res.send({ curso: `ID : ${id}` });
+});
+server.get("/curso/", (req, res) => {
+  const nome = req.query.nome;
+
+  return res.send({ curso: `Query  : ${nome}` });
 });
 
 server.listen(3000);
